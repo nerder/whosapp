abde977c6f3ae5a17991f27328c3b14fba4b8781
Stefano Saitta <stefano@buildo.io>
2016-03-21 22:14:49 +0100

    Refactoring the code in order to use CasperJS

--- whosapp.js
+++ whosapp.js
@@ -1,25 +1,57 @@
-var system = require('system');
-
-var page = new WebPage();
-page.settings.userAgent ='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.1500.71 Safari/537.36';
-page.viewportSize = {width: 1280, height: 1024};
-console.log(page.settings.userAgent);
-page.open('https://web.whatsapp.com/', function (status) {
-    if (status !== 'success') {
-      output.errors.push('Unable to access network');
-    } else {
-      window.setTimeout(function () {
-        page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js",function() {
-            page.clipRect = {
-              top: 214,
-              left: 296,
-              width: 250,
-              height: 250
-          }
-          page.render('whatsapp_qrcode.jpeg', {format: 'jpeg', quality: '100'});
-          console.log('QrCode Saved!');
-          phantom.exit();
-        });
-      }, 10500);
-    }
+var casper = require('casper').create({
+  clientScripts:  [
+      'node_modules/jquery/dist/jquery.min.js'
+  ],
+  pageSettings: {
+    userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.1500.71 Safari/537.36'
+  },
+  viewportSize: {
+    width: 1280,
+    height: 1024
+  },
+  waitTimeout: 15000,
+  logLevel: 'debug',
+  verbose: true
 });
+
+// http://docs.casperjs.org/en/latest/events-filters.html#remote-message
+casper.on("remote.message", function(msg) {
+    this.echo("Console: " + msg);
+});
+
+// http://docs.casperjs.org/en/latest/events-filters.html#page-error
+casper.on("page.error", function(msg, trace) {
+    this.echo("Error: " + msg);
+    this.echo("Trace:" + JSON.stringify(trace, undefined, 4));
+    // maybe make it a little fancier with the code from the PhantomJS equivalent
+});
+
+// http://docs.casperjs.org/en/latest/events-filters.html#resource-error
+casper.on("resource.error", function(resourceError) {
+    this.echo("ResourceError: " + JSON.stringify(resourceError, undefined, 4));
+});
+
+// http://docs.casperjs.org/en/latest/events-filters.html#page-initialized
+casper.on("page.initialized", function(page) {
+    // CasperJS doesn't provide `onResourceTimeout`, so it must be set through
+    // the PhantomJS means. This is only possible when the page is initialized
+    page.onResourceTimeout = function(request) {
+        console.log('Response Timeout (#' + request.id + '): ' + JSON.stringify(request));
+    };
+});
+
+casper.start('https://web.whatsapp.com/', function(){
+    this.echo('Starting...')
+    this.waitForSelector('img', function() {
+        this.capture('qrcode.png',
+          {
+            top: 214,
+            left: 296,
+            width: 250,
+            height: 250
+          }
+        );
+        this.echo('QR Screeshot Taken!')
+    });
+});
+casper.run();
